df3887e4434b4ce9ce031eaeb724a6274b05e742
Kyungyun Kim <kyknn79@naver.com>
2020-08-04 04:26:14 +0900

    chapter2 finish

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,21 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     # id는 장고에서 알아서 만들어줌
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # Question이라는 모델을 참조하겠다
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
