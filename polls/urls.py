83247055304740ebe44e8c5a6990527ec8bb3cb4
Kyungyun Kim <kyknn79@naver.com>
2020-08-04 16:54:57 +0900

    chapter3 finish

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name='polls'
 urlpatterns = [
     path('', views.index, name='index'), # view.index라는 view 내부로 연결시킴
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
